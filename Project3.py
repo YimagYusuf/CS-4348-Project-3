3be6d5c78c33412700e594fd3a7ea099667411e0
YimagYusuf <158852196+YimagYusuf@users.noreply.github.com>
2024-12-08 21:14:46 -0600

    Fixed Btree along with insert and search for the btree

--- Project3.py
+++ Project3.py
@@ -12,12 +12,13 @@ class BTreeNode:
         self.keyCount = 0
 
 class BTree:
-    blockSize = 512
-    magicNumber = b'4337PRJ3'
+    BLOCK_SIZE = 512
+    MAGIC = b'4337PRJ3'
 
     def __init__(self, filename=None):
         self.file = None
-        self.header = {'magic': self.magicNumber, 'rootId': 0, 'nextId': 1}
+        self.rootId = 0 
+        self.nextId = 1
         self.filename = filename
 
 
@@ -29,19 +30,18 @@ class BTree:
         
         if os.path.exists(filename):
             
-            overwrite = input(f"{filename}' exists do you want to overwrite? (yes/no): ").strip().lower()
+            overwrite = input(f"{filename}' exists do you want to overwrite it? (yes/no): ").strip().lower()
             
             if overwrite != 'yes':
-                print("Ended")
                 return
             
-        with open(filename, 'wb') as f:
-            f.write(self.header['magic'])
+        with open(filename, 'wb') as userFile:
+            userFile.write(self.MAGIC)
             
-            f.write(self.intToByte(self.header['rootId']))
-            f.write(self.intToByte(self.header['nextId']))
+            userFile.write(self.intToByte(self.rootId))
+            userFile.write(self.intToByte(self.nextId))
             
-            f.write(b'\x00' * (self.blockSize - 24))
+            userFile.write(b'\x00' * (self.BLOCK_SIZE - 24))
             
         self.openFile(filename)
         print(f"{filename} created and opened.")
@@ -57,9 +57,9 @@ class BTree:
             return
         
         self.filename = filename
-        with open(filename, 'rb') as f:
-            magic = f.read(8)
-            if magic != self.magicNumber:
+        with open(filename, 'rb') as userFile:
+            magic = userFile.read(8)
+            if magic != self.MAGIC:
                 print("Error invalid File")
                 return
             
@@ -67,18 +67,18 @@ class BTree:
         
         self.file.seek(8)
         
-        self.header['rootId'] = self.byteToInt(self.file.read(8))
-        self.header['nextId'] = self.byteToInt(self.file.read(8))
+        self.rootId = self.byteToInt(self.file.read(8))
+        self.nextId = self.byteToInt(self.file.read(8))
+        
         
         print(f"{filename} opened")
 
-    
 
 
 
     def readNode(self, blockId):
         
-        self.file.seek(blockId * self.blockSize)
+        self.file.seek(blockId * self.BLOCK_SIZE)
         
         node = BTreeNode()
         
@@ -105,12 +105,11 @@ class BTree:
         return node
 
 
-    
 
 
     def writeNode(self, node):
         
-        self.file.seek(node.id * self.blockSize)
+        self.file.seek(node.id * self.BLOCK_SIZE)
         
         self.file.write(self.intToByte(node.id))
         self.file.write(self.intToByte(node.parentId))
@@ -129,69 +128,218 @@ class BTree:
 
 
 
+
+
+
+
+
     def insert(self, key, value):
         if not self.file:
-            print("Error there is no file opened")
+            print("Error: No file is opened.")
             return
-        
-        if self.header['rootId'] == 0: 
+    
+        if self.rootId != 0:  
+            
+            root = self.readNode(self.rootId)
+            if root.keyCount == 19:  
+                
+                rootNode = BTreeNode()
+                rootNode.id = self.nextId
+                self.nextId += 1
+                
+                
+                rootNode.children[0] = root.id
+    
+                
+                middle = 9
+                splitNode = BTreeNode()
+                splitNode.id = self.nextId
+                self.nextId += 1
+                splitNode.parentId = rootNode.id
+                splitNode.keyCount = middle
+    
+                
+                
+                for i in range(middle):
+                    splitNode.keys[i] = root.keys[middle + i + 1]
+                    splitNode.values[i] = root.values[middle + i + 1]
+                
+                
+                if root.children[0] != 0:
+                    for i in range(middle + 1):
+                        splitNode.children[i] = root.children[middle + i + 1]
+
+    
+                
+                root.keyCount = middle
+    
+                
+                rootNode.keys[0] = root.keys[middle]
+                rootNode.values[0] = root.values[middle]
+                rootNode.children[1] = splitNode.id
+                rootNode.keyCount = 1
+    
+                
+                self.writeNode(root)
+                self.writeNode(splitNode)
+                self.writeNode(rootNode)
+    
+               
+                self.rootId = rootNode.id
+                self.file.seek(8)
+                self.file.write(self.intToByte(self.rootId))
+                self.file.write(self.intToByte(self.nextId))
+                self.file.flush()
+    
+               
+                if key > rootNode.keys[0]:
+                    self.insertKey(splitNode, key, value)
+                else:
+                    self.insertKey(root, key, value)
+            else:
+                self.insertKey(root, key, value)
+            print(f"Inserted: {key}, {value}")
+            
+        else:
             root = BTreeNode()
-            
-            root.id = self.header['nextId']
-            self.header['nextId'] += 1
-            
+            root.id = self.nextId
+            self.nextId += 1
             root.keys[0] = key
             root.values[0] = value
-            
             root.keyCount = 1
-            self.header['rootId'] = root.id
-            
+            self.rootId = root.id
             self.writeNode(root)
-            
             self.file.seek(8)
-            
-            self.file.write(self.intToByte(self.header['rootId']))
-            self.file.write(self.intToByte(self.header['nextId']))
-            
+            self.file.write(self.intToByte(self.rootId))
+            self.file.write(self.intToByte(self.nextId))
             self.file.flush()
+            print(f"Inserted: {key}, {value}")
             
-            print(f"Inserted:{key}, {value}")
-        else:
-            root = self.readNode(self.header['rootId'])
-            self.insertToNode(root, key, value)
-
 
 
 
     def search(self, key):
         if not self.file:
-            print("Error no file opened")
+            print("Error no file is opened")
             return
-        current = self.readNode(self.header['rootId'])
+        current = self.readNode(self.rootId)
         while current:
             found = False
             for i in range(current.keyCount):
                 if key == current.keys[i]:
                     print(f"{key}, {current.values[i]}")
                     return
-            current = None
-    
+                if key < current.keys[i]:
+                    childIndex = current.children[i]
+                    if childIndex != 0:
+                        current = self.readNode(childIndex)
+                    else:
+                        current = None
+                    found = True
+                    break
+            if not found:
+                keyCount = current.keyCount
+                if current.children[keyCount] != 0:
+                    current = self.readNode(current.children[keyCount])
+                else:
+                    current = None
         print("Key not found.")
 
 
 
-
-    def insertToNode(self, node, key, value):
+    def insertKey(self, node, key, value):
         i = node.keyCount - 1
-        while i >= 0 and key < node.keys[i]:
-            node.keys[i + 1] = node.keys[i]
-            node.values[i + 1] = node.values[i]
-            i -= 1
-        node.keys[i + 1] = key
-        node.values[i + 1] = value
-        node.keyCount += 1
-        self.writeNode(node)
-       
+    
+        
+        if node.children[0] == 0:
+            if node.keyCount == 19:
+                print(f"Full Node.")
+                return
+            
+            while i >= 0 and key < node.keys[i]:
+                node.keys[i + 1] = node.keys[i]
+                node.values[i + 1] = node.values[i]
+                i -= 1
+                
+            node.keys[i + 1] = key
+            node.values[i + 1] = value
+            node.keyCount += 1
+            self.writeNode(node)
+        else:
+            
+            while i >= 0 and key < node.keys[i]:
+                i -= 1
+            i += 1
+            child = self.readNode(node.children[i])
+            if child.keyCount == 19:
+                
+                splitNode = BTreeNode()
+                splitNode.id = self.nextId
+                self.nextId += 1
+                splitNode.parentId = node.id
+                middle = 9
+    
+                splitNode.keyCount = middle
+                for j in range(middle):
+                    splitNode.keys[j] = child.keys[middle + 1 + j]
+                    splitNode.values[j] = child.values[middle + 1 + j]
+                
+                
+                if child.children[0] != 0:
+                    for j in range(middle + 1):
+                        splitNode.children[j] = child.children[middle + 1 + j]
+
+    
+                
+                child.keyCount = middle
+    
+                
+                for j in range(node.keyCount, i, -1):
+                    node.children[j + 1] = node.children[j]
+                node.children[i + 1] = splitNode.id
+    
+                for j in range(node.keyCount - 1, i - 1, -1):
+                    node.keys[j + 1] = node.keys[j]
+                    node.values[j + 1] = node.values[j]
+    
+                node.keys[i] = child.keys[middle]
+                node.values[i] = child.values[middle]
+                node.keyCount += 1
+    
+                
+                self.writeNode(child)
+                self.writeNode(splitNode)
+                self.writeNode(node)
+    
+                
+                if key > node.keys[i]:
+                    child = self.readNode(node.children[i + 1])
+    
+            
+            self.insertKey(child, key, value)
+    
+    
+    
+    def printTree(self, blockId=None,):
+        
+        if blockId is None:
+            blockId = self.rootId
+            
+        if blockId == 0:
+            print("Nothing in the Tree")
+            return
+        
+        node = self.readNode(blockId) 
+        print(f"Node {blockId}:")
+        
+        for i in range(node.keyCount):
+            print(f"Key = {node.keys[i]}, Val = {node.values[i]}")
+    
+        for i in range(node.keyCount + 1):
+            
+            if node.children[i] != 0:
+                self.printTree(node.children[i])
+
 
 
 
@@ -203,20 +351,14 @@ class BTree:
 
 
 
+
     def byteToInt(self, value):
         answer = int.from_bytes(value, 'big')
         return answer
         
-  
 
-    def printTree(self, blockId=None, level=0):
-        if blockId is None:
-            blockId = self.header['rootId']
-        if blockId == 0:
-            print("Tree is empty.")
-            return
-        node = self.readNode(blockId)
-        print(f"{blockId}: Keys={node.keys[:node.keyCount]}")
+
+
 
 
 
